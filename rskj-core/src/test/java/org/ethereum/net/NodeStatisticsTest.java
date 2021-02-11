4c6d8d826bba6836bae50d4a8c189bf96f71e8e3
Nazaret Garcia <nazaret@iovlabs.org>
2021-02-11 18:20:49 -0300

    Updating tests

--- rskj-core/src/test/java/org/ethereum/net/NodeStatisticsTest.java
+++ rskj-core/src/test/java/org/ethereum/net/NodeStatisticsTest.java
@@ -2,23 +2,11 @@ package org.ethereum.net;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(NodeStatistics.class)
 public class NodeStatisticsTest {
 
     @Test
     public void getSessionFairReputation_OK() {
-        NodeStatistics nodeStatistics = new NodeStatistics();
-
-        Assert.assertEquals(0, nodeStatistics.getSessionFairReputation());
-    }
-
-    @Test
-    public void getSessionFairReputation_disconnected_OK() {
         NodeStatistics nodeStatistics = new NodeStatistics();
 
         Assert.assertEquals(0, nodeStatistics.getSessionFairReputation());
