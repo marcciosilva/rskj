9807efe46afc60d83d835c67673f0abaf160d5ba
fedejinich <federico@iovlabs.org>
2021-05-04 13:51:45 -0300

    added json rpc eth_getProof (introduced ProofDTO)

---
+++ rskj-core/src/main/java/co/rsk/rpc/modules/eth/getProof/ProofDTO.java
@@ -0,0 +1,61 @@
+package co.rsk.rpc.modules.eth.getProof;
+
+import java.util.List;
+
+public class ProofDTO {
+
+    private String balance;
+    private String codeHash;
+    private String nonce;
+    private String storageHash;
+    private List<String> accountProof;
+    private List<StorageProofDTO> storageProof;
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getCodeHash() {
+        return codeHash;
+    }
+
+    public void setCodeHash(String codeHash) {
+        this.codeHash = codeHash;
+    }
+
+    public String getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(String nonce) {
+        this.nonce = nonce;
+    }
+
+    public String getStorageHash() {
+        return storageHash;
+    }
+
+    public void setStorageHash(String storageHash) {
+        this.storageHash = storageHash;
+    }
+
+    public List<String> getAccountProof() {
+        return accountProof;
+    }
+
+    public void setAccountProof(List<String> accountProof) {
+        this.accountProof = accountProof;
+    }
+
+    public List<StorageProofDTO> getStorageProof() {
+        return storageProof;
+    }
+
+    public void setStorageProof(List<StorageProofDTO> storageProof) {
+        this.storageProof = storageProof;
+    }
+}
